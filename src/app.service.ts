ec3c71597682cc6d5d3951940e16a13a5cc44280
Michael <guay.me@gmail.com>
2022-12-18 18:43:38 -0500

    Autoscaling

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import * as fibonacci from 'fibonacci';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): number {
+    return fibonacci.iterate(process.env.FIBONACCI || 3000);
   }
 }
